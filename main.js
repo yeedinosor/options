03c9d5ff7eccbc04cd3fa27f6728a1c525af4076
yeedinosor <ethan.xu.3.1415@gmail.com>
2024-02-20 18:28:21 -0800

    stock data added

---
+++ main.js
@@ -0,0 +1,15 @@
+const url = 'http://73.162.165.166:3000/';
+
+fetch(url, {method: 'GET'})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+})
+.then(data => {
+  document.getElementById("demo").innerHTML = JSON.stringify(data['chart']['result']);
+})
+.catch(error => {
+  console.error('There was a problem with your fetch operation:', error);
+});
---
+++ proxy-server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+
+
+const url = 'https://query1.finance.yahoo.com/v8/finance/chart/AAPL?interval=1d';
+const headers = new Headers({
+  'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+  'Accept-Encoding': 'none',
+  'Accept-Language': 'en-US,en;q=0.8',
+  'Connection': 'keep-alive'
+});
+
+var returnData;
+
+fetch(url, {
+  method: 'GET',
+  headers: headers,
+  mode: 'no-cors'
+})
+.then(response => {
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.text(); 
+})
+.then(data => {
+  //console.log(data['chart']['result']);
+  returnData = data;
+})
+.catch(error => {
+  console.error('There was a problem with your fetch operation:', error);
+});
+
+const server = http.createServer((req, res) => {
+res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  console.log(`${req.method} ${req.url}`);
+
+  //res.end(JSON.stringify(returnData));
+  res.end(returnData);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Proxy server listening on port ${PORT}`);
+});
